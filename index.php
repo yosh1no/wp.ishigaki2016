ab908de95fee5d1be0e9c77147cb929ebf1a7ca4
Yoshino Ryuki <e16006@std.it-college.ac.jp>
2016-09-05 21:44:41 +0900

    テンプレートの分割を行う

--- index.php
+++ index.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css" type="text/css" />
-<!--[if lt IE 9]>
-<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<script src="https://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-<![endif]-->
-<title><?php bloginfo('name'); ?></title>
-</head>
-<body <?php body_class();?>>
-    <header class="globalHeader">
-        <div class="inner">
-            <h1><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo01.png" height="40" width="300" alt="ホテル・技評リゾート石垣島"></a></h1>
-            <p class="description"><?php bloginfo('description'); ?></p>
-            <form action="" method="get" class="searchform">
-                <input type="text" value="" name="s" id="s" placeholder="サイト内検索">
-                <input type="submit" id="searchSubmit" value="">
-            </form>
-        </div>
-    </header><!-- /.globalHeader -->
-
-    <div class="homeVisual"><span>石垣島でのんびりゆったりと。</span></div>
-
-    <nav class="globalNavi">
-        <ul>
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="about.html">ホテル紹介</a></li>
-            <li><a href="access.html">アクセス</a></li>
-            <li><a href="contact.html">お問い合わせ</a></li>
-        </ul>
-    </nav><!-- /.globalNavi -->
-
+<?php get_header(); ?>
     <div class="contentsWrap">
         <div class="mainContents">
 
@@ -112,36 +79,8 @@
         </div><!-- /.mainContents -->
 
         <aside class="subContents">
-            <p class="bnr">
-                <a href="contact.html"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_contact01.png" height="183" width="300" alt="宿泊のお問い合わせ"></a>
-            </p>
-            <div class="wrapper">
-                <section class="socialBanners">
-                    <h2 class="hidden">関連バナー</h2>
-                    <div class="banners">
-                        <ul>
-                            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_fb01.png" height="60" width="290" alt=""></a></li>
-                            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_tw01.png" height="60" width="290" alt=""></a></li>
-                            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_rss01.png" height="60" width="290" alt=""></a></li>
-                        </ul>
-                    </div>
-                </section><!-- /.socialBanners -->
-            </div><!-- /.wrapper -->
+          <?php get_sidebar(); ?>
         </aside><!-- /.subContents -->
     </div><!-- /.contentsWrap -->
 
-    <footer class="globalFooter">
-        <div class="pageTop">
-            <p><a href="javascript:void(0);" id="js-pagetop"><img src="<?php echo get_template_directory_uri(); ?>/images/common/pagetop01.png" height="41" width="41" alt=""></a></p>
-        </div>
-        <div class="inner">
-            <dl class="address">
-                <dt>ホテル・技評リゾート石垣島</dt>
-                <dd>〒123-4567 沖縄県石垣市0123</dd>
-                <dd>TEL 0123(45)6789 / FAX 0123(45)6789</dd>
-            </dl><!-- /.address -->
-            <small>&copy; HOTEL GIHYO RESORT ISHIGAKI All Rights Reserved.</small>
-        </div>
-    </footer><!-- /.globalFooter -->
-</body>
-</html>
+<?php get_footer(); ?>
