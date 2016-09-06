6e451a1758b9dd8cb9b2d60fc2c152d21be9ab22
Yoshino Ryuki <e16006@std.it-college.ac.jp>
2016-09-06 21:06:10 +0900

    wp_head関数の追加

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ bloginfo('name');
 <?php
 wp_enqueue_script('jquery');
 wp_enqueue_script('hotel-common',get_template_directory_uri() . '/js/common.js');
+wp_head();
 ?>
 </head>
 <body <?php body_class(); ?>>
