8aa70a959975f9b460e578fa1e751bfdea1bb2b0
Yoshino Ryuki <e16006@std.it-college.ac.jp>
2016-09-20 20:29:43 +0900

    RSS関連のカスタマイズ

--- functions.php
+++ functions.php
@@ -21,4 +21,32 @@ function my_comment_form_defoult_fields( $args ) {
  * head内にRSSのlink要素を出力する
  */
 add_theme_support( 'automatic-feed-links' );
+/**
+ *RSSの文字数を調整
+ */
+add_filter('excerpt_mblength','my_excerpt_mblength');
+function my_excerpt_mblength( $length ) {
+  return 20; // 抜粋に出力する文字数
+}
+/**
+ *RSSにアイキャッチ画像を出力する
+ */
+ function rss_post_thumbnail( $content) {
+     global $post;
+     if (has_post_thumbnail( $post->ID)) {
+         $content = '<p>' . get_the_post_thumbnail($post->ID) .'</p>' . $content;
+     }
+     return $content;
+ }
+ add_filter( 'the_excerpt_rss',  'rss_post_thumbnail');
+ add_filter( 'the_content_feed', 'rss_post_thumbnail');
+/**
+ *RSSに「続きを読む」のリンクを追加する
+ */
+add_filter('excerpt_more','excerpt_more');
+function my_excerpt_more( $more ) {
+  return '...<a href="'. get_permalink( get_the_ID() ) . '">続きを読む→</a>';
+}
+//RSSの配信を止める
+// remove_action('do_feed_rss2', 'do_feed_rss2', 10, 1);
 ?>
